0ef1333613558aa7e11f330a47c960644870b0d8
xiaolizideshuyu <31614412@qq.com>
2024-10-27 10:34:54 +0800

    opencv学习

--- struct_no_sign_integer.py
+++ struct_no_sign_integer.py
@@ -1,8 +1,9 @@
 import struct
+
 """负数的处理"""
 
 a = 1
-b = -1
+b = -2
 
 # 打包
 aa = struct.pack('>i', a)
@@ -11,7 +12,9 @@ print(aa, aa.hex())
 print(bb, bb.hex())
 
 # 解包
-aaa=struct.unpack('>i' , aa)
-bbb=struct.unpack('>i',bb)
+aaa = struct.unpack('>i', aa)
+bbb = struct.unpack('>i', bb)
 print(aaa)
 print(bbb)
+print('22222222222222222')
+print(format(255, 'b') ,format(-255, 'b'))
